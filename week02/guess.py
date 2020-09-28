b9d69909eedb58dbabc4745e0f347ac545c899a8
Bannngo <mak.meow@outlook.com>
2020-09-29 00:34:12 +0800

    上传第二周练习

---
+++ week02/guess.py
@@ -0,0 +1,26 @@
+print("可恶的吉祥话猜猜游戏")
+temp = input("请输入1-10其中一个数字")
+a = int(temp)
+
+if a == 1:
+    print("祝你一帆风顺")
+elif a == 2:
+    print("祝你双喜临门")
+elif a == 3:
+    print("祝你三年抱两")
+elif a == 4:
+    print("祝你四世同堂")
+elif a == 5:
+    print("祝你五福临门")
+elif a == 6:
+    print("祝你六六大顺")
+elif a == 7:
+    print("祝你七星高照")
+elif a == 8:
+    print("祝你发发发发")
+elif a == 9:
+    print("祝你九天揽月")
+elif a == 10:
+    print("祝你十全十美")
+else:
+    print("不是说了1-10之内吗")
