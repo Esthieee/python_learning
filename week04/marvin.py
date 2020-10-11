7137b3f509acf97ee2afb1871e809fa61a3e9f36
Bannngo <mak.meow@outlook.com>
2020-10-11 22:56:37 +0800

    上传作业

---
+++ week04/marvin.py
@@ -0,0 +1,14 @@
+
+paranoid_andriod = 'Marvin, the paranoid andriod'
+letters = list(paranoid_andriod)
+#for char in letters:
+#    print('\t', char)
+
+for char in letters[:6]:
+    print('\t', char)
+print()
+for char in letters[-7:]:
+    print('\t'*2, char)
+print()
+for char in letters[12:20]:
+    print('\t'*3, char)
